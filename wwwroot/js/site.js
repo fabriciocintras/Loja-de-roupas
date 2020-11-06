d9137bbe6f91f312d6001dac8eddf7da906de8eb
fabricio cintra <fabricioalvescintra@gmail.com>
2020-11-06 01:29:55 -0300

    commit Projeto Integrador

---
+++ wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿document.getElementById("abrirMenu").onclick = function(){
+    document.querySelector("#menuResponsivo").classList.add("show");
+}
+
+document.getElementById("sairMenu").onclick = function(){
+    document.querySelector("#menuResponsivo").classList.remove("show");
+}
